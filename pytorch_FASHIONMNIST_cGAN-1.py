914e45264265023430a93d1778c9b567b8fa4dd3
zhang-zx <zhangzx15@126.com>
2018-04-28 11:28:50 +0800

    fix bugs

--- pytorch_FASHIONMNIST_cGAN-1.py
+++ pytorch_FASHIONMNIST_cGAN-1.py
@@ -1,6 +1,8 @@
 import os, time
-import matplotlib.pyplot as plt
-plt.switch_backend('agg') 
+import matplotlib.pyplot as plt 
+plt.switch_backend('agg')
+
+
 import itertools
 import pickle
 import imageio
@@ -11,112 +13,65 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.autograd import Variable
 
-# G(z)
-class generator(nn.Module):
-    # initializers
-    def __init__(self):
-        super(generator, self).__init__()
-        self.fc1_1 = nn.Linear(100, 256)
-        self.fc1_1_bn = nn.BatchNorm1d(256)
-        self.fc1_2 = nn.Linear(10, 256)
-        self.fc1_2_bn = nn.BatchNorm1d(256)
-        self.fc2 = nn.Linear(512, 512)
-        self.fc2_bn = nn.BatchNorm1d(512)
-        self.fc3 = nn.Linear(512, 1024)
-        self.fc3_bn = nn.BatchNorm1d(1024)
-        self.fc4 = nn.Linear(1024, 784)
 
-    # weight_init
-    def weight_init(self, mean, std):
-        for m in self._modules:
-            normal_init(self._modules[m], mean, std)
 
-    # forward method
-    def forward(self, input, label):
-        x = F.relu(self.fc1_1_bn(self.fc1_1(input)))
-        y = F.relu(self.fc1_2_bn(self.fc1_2(label)))
-        x = torch.cat([x, y], 1)
-        x = F.relu(self.fc2_bn(self.fc2(x)))
-        x = F.relu(self.fc3_bn(self.fc3(x)))
-        x = F.tanh(self.fc4(x))
 
-        return x
+# training parameters
+batch_size = 128
+lr = 0.0002
+train_epoch = 50
 
-class discriminator(nn.Module):
-    # initializers
-    def __init__(self):
-        super(discriminator, self).__init__()
-        self.fc1_1 = nn.Linear(784, 1024)
-        self.fc1_2 = nn.Linear(10, 1024)
-        self.fc2 = nn.Linear(2048, 512)
-        self.fc2_bn = nn.BatchNorm1d(512)
-        self.fc3 = nn.Linear(512, 256)
-        self.fc3_bn = nn.BatchNorm1d(256)
-        self.fc4 = nn.Linear(256, 1)
+# load data
+transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))])
+trainSet = datasets.FashionMNIST('data', train=True, download=True, transform=transform)
+train_loader = torch.utils.data.DataLoader(trainSet, batch_size=batch_size, shuffle=True)
 
-    # weight_init
-    def weight_init(self, mean, std):
-        for m in self._modules:
-            normal_init(self._modules[m], mean, std)
+# testSet = datasets.FashionMNIST('data', train=False, download=True, transform=transform)
+# test_loader = torch.utils.data.DataLoader(test_loader, batch_size=batch_size, shuffle=False)
 
-    # forward method
-    def forward(self, input, label):
-        x = F.leaky_relu(self.fc1_1(input), 0.2)
-        y = F.leaky_relu(self.fc1_2(label), 0.2)
-        x = torch.cat([x, y], 1)
-        x = F.leaky_relu(self.fc2_bn(self.fc2(x)), 0.2)
-        x = F.leaky_relu(self.fc3_bn(self.fc3(x)), 0.2)
-        x = F.sigmoid(self.fc4(x))
 
-        return x
+temp_z = torch.rand(20, 100)
+fixed_z = temp_z
+fixed_y = torch.zeros(20, 1)
 
-def normal_init(m, mean, std):
-    if isinstance(m, nn.Linear):
-        m.weight.data.normal_(mean, std)
-        m.bias.data.zero_()
-
-temp_z_ = torch.rand(10, 100)
-fixed_z_ = temp_z_
-fixed_y_ = torch.zeros(10, 1)
 for i in range(9):
-    fixed_z_ = torch.cat([fixed_z_, temp_z_], 0)
-    temp = torch.ones(10,1) + i
-    fixed_y_ = torch.cat([fixed_y_, temp], 0)
+    fixed_z = torch.cat([fixed_z, temp_z], 0)
+    temp = torch.ones(20,1) + i
+    fixed_y = torch.cat([fixed_y, temp], 0)
 
+fixed_z = Variable(fixed_z.cuda(), volatile=True)
+fixed_y_label = torch.zeros(200, 10)
+fixed_y_label.scatter_(1, fixed_y.type(torch.LongTensor), 1)
+fixed_y_label = Variable(fixed_y_label.cuda(), volatile=True)
 
-fixed_z_ = Variable(fixed_z_.cuda(), volatile=True)
-fixed_y_label_ = torch.zeros(100, 10)
-fixed_y_label_.scatter_(1, fixed_y_.type(torch.LongTensor), 1)
-fixed_y_label_ = Variable(fixed_y_label_.cuda(), volatile=True)
-
-def show_result(num_epoch, show = False, save = False, path = 'result.png'):
-
+# 10(classes) * 20(pics)
+def show_result(epoch_count, show = False, save = True, path = './'):
     G.eval()
-    test_images = G(fixed_z_, fixed_y_label_)
+    result = G(fixed_z, fixed_y_label)
     G.train()
-
-    size_figure_grid = 10
-    fig, ax = plt.subplots(size_figure_grid, size_figure_grid, figsize=(5, 5))
-    for i, j in itertools.product(range(size_figure_grid), range(size_figure_grid)):
+    x_size = 20
+    y_size = 10
+    fig, ax = plt.subplots(y_size, x_size, figsize=(10, 5))
+    for i, j in itertools.product(range(x_size), range(y_size)):
         ax[i, j].get_xaxis().set_visible(False)
         ax[i, j].get_yaxis().set_visible(False)
-
-    for k in range(10*10):
-        i = k // 10
+    for k in range(200):
+        i = k // 20
         j = k % 10
         ax[i, j].cla()
-        ax[i, j].imshow(test_images[k].cpu().data.view(28, 28).numpy(), cmap='gray')
-
-    label = 'Epoch {0}'.format(num_epoch)
+        ax[i, j].imshow(result[k].cpu().data.view(28, 28).numpy(), cmap='gray')
+    label = 'Epoch {0}'.format(epoch_count)
     fig.text(0.5, 0.04, label, ha='center')
-    plt.savefig(path)
-
+    if save:
+        plt.savefig(path)
     if show:
         plt.show()
     else:
         plt.close()
 
-def show_train_hist(hist, show = False, save = False, path = 'Train_hist.png'):
+def process(hist, show = False, save = True, path = 'Train.png'):
     x = range(len(hist['D_losses']))
 
     y1 = hist['D_losses']
@@ -140,49 +95,98 @@ def show_train_hist(hist, show = False, save = False, path = 'Train_hist.png'):
     else:
         plt.close()
 
-# training parameters
-batch_size = 128
-lr = 0.0002
-train_epoch = 50
+def normal_init(m, mean, std):
+    if isinstance(m, nn.Linear):
+        m.weight.data.normal_(mean, std)
+        m.bias.data.zero_()
 
-# data_loader
-transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
-])
-train_loader = torch.utils.data.DataLoader(
-    datasets.FashionMNIST(root='./', train=True, transform=transform, target_transform=None, download=True),
-    batch_size=batch_size, shuffle=True)
+class generator(nn.Module):
+    """docstring for generator"""
+    def __init__(self):
+        super(generator, self).__init__()
+        self.fc1_1 = nn.Linear(100, 256)
+        self.fc1_1_bn = nn.BatchNorm1d(256)
+        self.fc1_2 = nn.Linear(10, 256)
+        self.fc1_2_bn = nn.BatchNorm1d(256)
+        self.fc2 = nn.Linear(512, 512)
+        self.fc2_bn = nn.BatchNorm1d(512)
+        self.fc3 = nn.Linear(512, 1024)
+        self.fc3_bn = nn.BatchNorm1d(1024)
+        self.fc4 = nn.Linear(1024, 784)
 
-# network
+    def weight_init(self, mean, std):
+        for module in self._modules:
+            normal_init(self._modules[module], mean, std)
+
+    def forward(self, input_data, label):
+        z = F.relu(self.fc1_1_bn(self.fc1_1(input_data)))
+        y = F.relu(self.fc1_2_bn(self.fc1_2(label)))
+        z = torch.cat([z, y], 1)
+        z = F.relu(self.fc2_bn(self.fc2(z)))
+        z = F.relu(self.fc3_bn(self.fc3(z)))
+        z = F.tanh(self.fc4(z))
+        return z
+
+
+class discriminator(nn.Module):
+    """docstring for discriminator"""
+    def __init__(self):
+        super(discriminator, self).__init__()
+        self.fc1_1 = nn.Linear(784, 1024)
+        # self.fc1_1_bn = nn.BatchNorm1d(1024)
+        self.fc1_2 = nn.Linear(10, 1024)
+        # self.fc1_2_bn = nn.BatchNorm1d(1024)
+        self.fc2 = nn.Linear(2048, 512)
+        self.fc2_bn = nn.BatchNorm1d(512)
+        self.fc3 = nn.Linear(512, 256)
+        self.fc3_bn = nn.BatchNorm1d(256)
+        self.fc4 = nn.Linear(256, 1)
+        
+    def weight_init(self, mean, std):
+        for module in self._modules:
+            normal_init(self._modules[module], mean, std)
+
+    def forward(self, input_data, label):
+        x = F.leaky_relu(self.fc1_1(input_data), 0.2)
+        y = F.leaky_relu(self.fc1_2(label), 0.2)
+        x = torch.cat([x, y], 1)
+        x = F.leaky_relu(self.fc2_bn(self.fc2(x)), 0.2)
+        x = F.leaky_relu(self.fc3_bn(self.fc3(x)), 0.2)
+        x = F.sigmoid(self.fc4(x))
+        return x
+
 G = generator()
 D = discriminator()
 G.weight_init(mean=0, std=0.02)
 D.weight_init(mean=0, std=0.02)
+
+
 G.cuda()
 D.cuda()
 
 # Binary Cross Entropy loss
-BCE_loss = nn.BCELoss()
+BCELoss = nn.BCELoss()
 
 # Adam optimizer
 G_optimizer = optim.Adam(G.parameters(), lr=lr, betas=(0.5, 0.999))
 D_optimizer = optim.Adam(D.parameters(), lr=lr, betas=(0.5, 0.999))
 
 # results save folder
-if not os.path.isdir('FASHION_MNIST_cGAN_results'):
-    os.mkdir('FASHION_MNIST_cGAN_results')
-if not os.path.isdir('FASHION_MNIST_cGAN_results/Fixed_results'):
-    os.mkdir('FASHION_MNIST_cGAN_results/Fixed_results')
+if not os.path.isdir('FashionMNIST_results'):
+    os.mkdir('FashionMNIST_results')
+if not os.path.isdir('FashionMNIST_results/Fixed_results'):
+    os.mkdir('FashionMNIST_results/Fixed_results')
 
-train_hist = {}
-train_hist['D_losses'] = []
-train_hist['G_losses'] = []
-train_hist['per_epoch_ptimes'] = []
-train_hist['total_ptime'] = []
+hist = {}
+hist['D_losses'] = []
+hist['G_losses'] = []
+hist['epoch_time'] = []
+hist['total_time'] = 0
 
-print('training start!')
+
 start_time = time.time()
+print('training start ')
+
 for epoch in range(train_epoch):
     D_losses = []
     G_losses = []
@@ -197,88 +201,112 @@ for epoch in range(train_epoch):
         G_optimizer.param_groups[0]['lr'] /= 10
         D_optimizer.param_groups[0]['lr'] /= 10
         print("learning rate change!")
-
+    
     epoch_start_time = time.time()
-    for x_, y_ in train_loader:
-        # train discriminator D
+    for x, y in train_loader:
         D.zero_grad()
+        
+        batch = x.size()[0]
+        
+        y_real = torch.ones(batch)
+        y_fake = torch.zeros(batch)
+        
+        y_label = torch.zeros(batch, 10)
+        y_label.scatter_(1, y.view(batch, 1), 1)
 
-        mini_batch = x_.size()[0]
+        x = x.view(-1, 28 * 28)
 
-        y_real_ = torch.ones(mini_batch)
-        y_fake_ = torch.zeros(mini_batch)
-        y_label_ = torch.zeros(mini_batch, 10)
-        y_label_.scatter_(1, y_.view(mini_batch, 1), 1)
+        x, y_label, y_real, y_fake = Variable(x.cuda()), Variable(y_label.cuda()), Variable(y_real.cuda()), Variable(y_fake.cuda())
+        # train with real data
+        D_result = D(x, y_label).squeeze()
+        D_real_loss = BCELoss(D_result, y_real)
+        
+        # generate fake data
+        z = torch.rand((batch, 100))
+        y = (torch.rand(batch, 1) * 10).type(torch.LongTensor)
+        y_label = torch.zeros(batch, 10)
+        y_label.scatter_(1, y.view(batch, 1), 1)
+        
 
-        x_ = x_.view(-1, 28 * 28)
-        x_, y_label_, y_real_, y_fake_ = Variable(x_.cuda()), Variable(y_label_.cuda()), Variable(y_real_.cuda()), Variable(y_fake_.cuda())
-        D_result = D(x_, y_label_).squeeze()
-        D_real_loss = BCE_loss(D_result, y_real_)
+        z, y_label = Variable(z.cuda()), Variable(y_label.cuda())
 
-        z_ = torch.rand((mini_batch, 100))
-        y_ = (torch.rand(mini_batch, 1) * 10).type(torch.LongTensor)
-        y_label_ = torch.zeros(mini_batch, 10)
-        y_label_.scatter_(1, y_.view(mini_batch, 1), 1)
+        G_result = G(z, y_label)
+        # train with fake data
+        D_result = D(G_result, y_label).squeeze()
+        D_fake_loss = BCELoss(D_result, y_fake)
 
-        z_, y_label_ = Variable(z_.cuda()), Variable(y_label_.cuda())
-        G_result = G(z_, y_label_)
-
-        D_result = D(G_result, y_label_).squeeze()
-        D_fake_loss = BCE_loss(D_result, y_fake_)
-        D_fake_score = D_result.data.mean()
-
-        D_train_loss = D_real_loss + D_fake_loss
-
+        D_train_loss = D_fake_loss + D_real_loss
+        # optimize D
         D_train_loss.backward()
         D_optimizer.step()
-
+        # record the loss of D
         D_losses.append(D_train_loss.data[0])
 
-        # train generator G
+        # train G
         G.zero_grad()
 
-        z_ = torch.rand((mini_batch, 100))
-        y_ = (torch.rand(mini_batch, 1) * 10).type(torch.LongTensor)
-        y_label_ = torch.zeros(mini_batch, 10)
-        y_label_.scatter_(1, y_.view(mini_batch, 1), 1)
+        z = torch.rand((batch, 100))
+        y = (torch.rand(batch, 1) * 10).type(torch.LongTensor)
+        y_label = torch.zeros(batch, 10)
+        y_label.scatter_(1, y.view(batch, 1), 1)
 
-        z_, y_label_ = Variable(z_.cuda()), Variable(y_label_.cuda())
+        z, y_label = Variable(z.cuda()), Variable(y_label.cuda())
 
-        G_result = G(z_, y_label_)
-        D_result = D(G_result, y_label_).squeeze()
-        G_train_loss = BCE_loss(D_result, y_real_)
-        G_train_loss.backward()
+        G_result = G(z, y_label)
+        D_result = D(G_result, y_label).squeeze()
+        G_loss = BCELoss(D_result, y_real)
+        # optimize G
+        G_loss.backward()
         G_optimizer.step()
 
-        G_losses.append(G_train_loss.data[0])
+        G_losses.append(G_loss.data[0])
 
-    epoch_end_time = time.time()
-    per_epoch_ptime = epoch_end_time - epoch_start_time
+    epoch_time = time.time() - epoch_start_time
+    hist['D_losses'] += D_losses
+    hist['G_losses'] += G_losses
+    hist['epoch_time'].append(epoch_time)
 
-
-    print('[%d/%d] - ptime: %.2f, loss_d: %.3f, loss_g: %.3f' % ((epoch + 1), train_epoch, per_epoch_ptime, torch.mean(torch.FloatTensor(D_losses)),
+    print('[%d/%d] - ptime: %.2f, loss_d: %.3f, loss_g: %.3f' % ((epoch + 1), train_epoch, epoch_time, torch.mean(torch.FloatTensor(D_losses)),
                                                               torch.mean(torch.FloatTensor(G_losses))))
-    fixed_p = 'FASHION_MNIST_cGAN_results/Fixed_results/MNIST_cGAN_' + str(epoch + 1) + '.png'
-    show_result((epoch+1), save=True, path=fixed_p)
-    train_hist['D_losses'].append(torch.mean(torch.FloatTensor(D_losses)))
-    train_hist['G_losses'].append(torch.mean(torch.FloatTensor(G_losses)))
-    train_hist['per_epoch_ptimes'].append(per_epoch_ptime)
+    pic_path = 'FashionMNIST_results/Fixed_results/FashionMNIST_' + str(epoch + 1) + '.png'
+    show_result(epoch+1, path=pic_path)
 
-end_time = time.time()
-total_ptime = end_time - start_time
-train_hist['total_ptime'].append(total_ptime)
+total_time = time.time() - start_time
+hist['total_time'] = total_time
 
-print("Avg one epoch ptime: %.2f, total %d epochs ptime: %.2f" % (torch.mean(torch.FloatTensor(train_hist['per_epoch_ptimes'])), train_epoch, total_ptime))
-print("Training finish!... save training results")
-torch.save(G.state_dict(), "FASHION_MNIST_cGAN_results/generator_param.pkl")
-torch.save(D.state_dict(), "FASHION_MNIST_cGAN_results/discriminator_param.pkl")
-with open('FASHION_MNIST_cGAN_results/train_hist.pkl', 'wb') as f:
-    pickle.dump(train_hist, f)
+print("Avg one epoch time: %.2f, total %d epochs time: %.2f" % (torch.mean(torch.FloatTensor(train_hist['epoch_time'])), train_epoch, total_time))
+print("Training finished! saving the training results")
+torch.save(G.state_dict(), "FashionMNIST_results/generator_param.pkl")
+torch.save(D.state_dict(), "FashionMNIST_results/discriminator_param.pkl")
 
-show_train_hist(train_hist, save=True, path='FASHION_MNIST_cGAN_results/FASHION_MNIST_cGAN_train_hist.png')
+with open('FashionMNIST_results/train_hist.pkl', 'wb') as f:
+    pickle.dump(hist, f)
+
+process(hist, path='FashionMNIST_results/FashionMNIST_train_hist.png')
 
 images = []
+
 for e in range(train_epoch):
-    img_name = 'FASHION_MNIST_cGAN_results/Fixed_results/FASHION_MNIST_cGAN_' + str(e + 1) + '.png'
+    img_name = 'FashionMNIST_results/Fixed_results/FashionMNIST_' + str(e + 1) + '.png'
     images.append(imageio.imread(img_name))
-imageio.mimsave('FASHION_MNIST_cGAN_results/generation_animation.gif', images, fps=5)
+imageio.mimsave('FashionMNIST_results/generation_animation.gif', images, fps=3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
